9d8f91180549c96b13d16f560db9985949145a49
stormynight9 <ferjani.nader@hotmail.fr>
2022-02-28 20:13:53 +0100

    trying to fetch from an api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [movie, setMovie] = useState('yo')
+
+
+  useEffect(() => {
+
+    fetch('https://api.jikan.moe/v3/anime/1')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setMovie(data)
+      });
+  }, [])
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <p>{movie.mal_id}</p>
+      <img src={movie.image_url} alt={movie.title} />
+      <p>{movie.trailer_url}</p>
+      <p>{movie.title}</p>
+      <p>{movie.episodes}</p>
+    </div>
   );
 }
 
