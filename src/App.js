9b1b03e19be91b67528a13a2a2abaf03d2cc7b03
stormynight9 <ferjani.nader@hotmail.fr>
2022-02-28 21:33:29 +0100

    generating random movie works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { useEffect, useState } from "react";
 
+const randomNumber10 = () => Math.floor(Math.random() * 10 + 1)
+const randomNumber50 = () => Math.floor(Math.random() * 50)
+
 function App() {
 
   const [movie, setMovie] = useState('yo')
 
+  const fetchTopAnime = (randomNumber) => {
+    fetch(`https://api.jikan.moe/v3/top/anime/${randomNumber}/movie`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const rng50 = randomNumber50()
+        const animeId = data.top[rng50].mal_id
+        console.log(animeId)
+        fetchRandomAnime(animeId)
+      });
+  }
 
-  useEffect(() => {
-
-    fetch('https://api.jikan.moe/v3/anime/1')
+  const fetchRandomAnime = (animeId) => {
+    fetch(`https://api.jikan.moe/v3/anime/${animeId}`)
       .then(response => response.json())
       .then(data => {
         console.log(data)
         setMovie(data)
       });
+  }
+
+  useEffect(() => {
+    fetchTopAnime(randomNumber10())
   }, [])
+
+
+
 
   return (
     <div>
@@ -22,6 +42,8 @@ function App() {
       <p>{movie.trailer_url}</p>
       <p>{movie.title}</p>
       <p>{movie.episodes}</p>
+      <p>{randomNumber10()}</p>
+      <button className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">Random</button>
     </div>
   );
 }
